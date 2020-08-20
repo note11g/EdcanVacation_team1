7fda660fde179de42bd561999317a218950b0867
note11 <67783062+note11g@users.noreply.github.com>
2020-08-20 21:31:31 +0900

    20200820/2131

---
+++ app/src/main/java/com/edcan/shareformproject/HomeFragment.java
@@ -0,0 +1,40 @@
+package com.edcan.shareformproject;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.edcan.shareformproject.databinding.FragmentHomeBinding;
+
+
+public class HomeFragment extends Fragment {
+
+    public static HomeFragment newInstance(){
+        return new HomeFragment();
+    }
+
+    private Context mContext;
+
+    private FragmentHomeBinding binding;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container,false);
+        return binding.getRoot();
+    }
+}
---
+++ app/src/main/java/com/edcan/shareformproject/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.edcan.shareformproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, GoogleLoginActivity.class));
+            finish();
+        },1500);
+    }
+
+
+}
